5278edd86e97368bf333ce8aef1a4f6d94c354f8
deven <devenfan@gmail.com>
2016-06-24 12:04:57 +0800

    让编译出来的war包也可以在tomcat或jboss等web容器下使用

---
+++ src/main/java/com/mvmlabs/springboot/ServletInitializer.java
@@ -0,0 +1,17 @@
+package com.mvmlabs.springboot;
+
+/**
+ * Created by Deven on 2016-06-23.
+ */
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+
+}
+
